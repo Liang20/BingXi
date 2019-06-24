810d5a9f334dd47fd7b20cb9382a925b43bf6697
Administrator <3064725318@qq.com>
2019-06-24 23:42:41 +0800

    2.1品牌添加完成

---
+++ bingxi-pojo/src/main/java/com/bingxi/entity/PageResult.java
@@ -0,0 +1,35 @@
+package com.bingxi.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+//分页
+public class PageResult implements Serializable{
+	private long total;
+	private  List rows;
+	public long getTotal() {
+		return total;
+	}
+	public void setTotal(long total) {
+		this.total = total;
+	}
+	public List getRows() {
+		return rows;
+	}
+	public void setRows(List rows) {
+		this.rows = rows;
+	}
+	public PageResult() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public PageResult(long total, List rows) {
+		super();
+		this.total = total;
+		this.rows = rows;
+	}
+	
+	
+	
+
+}
